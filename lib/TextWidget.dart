47961eac249d1cfff19d3334cae762a416929878
chenfuwei <chenfuwei601@163.com>
2019-05-30 11:32:18 +0800

    文本截断的样式

--- lib/TextWidget.dart
+++ lib/TextWidget.dart
@@ -10,12 +10,13 @@ class TextWidget extends StatelessWidget
       child: new Column(
         children: <Widget>[
           new Text(
-            '加粗 24号 无斜线',
+            '加粗 24号 无斜线拉开始大幅拉升看到疯狂拉升的风格来看圣诞节复古镂空设计的礼服刚开始记录的开发工具路上看到飞机过来上课记得发过来看手机里的咖啡馆介绍了肯德基反过来思考江东父老顾客介绍来的反馈给家里开始觉得分开过',
             style: new TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 24,
                 decoration: TextDecoration.none,
             ),
+            overflow: TextOverflow.ellipsis,
           ),
           new Text(
             '加粗 24号 下斜虚线',
